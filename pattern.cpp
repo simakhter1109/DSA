374886236e59e411bf7730d9ef77135a31d00ebc
Sim Akhter <simakhter1109@gmail.com>
2026-08-14 00:53:40 +0530

    Add divisor and pattern solutions

---
+++ pattern.cpp
@@ -0,0 +1,42 @@
+// PATTERN 2
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// void print2(int n){
+//     for(int i = 0; i <n; i ++){
+//         for(int j= 0; j<n; j++){
+//         cout << " " ;
+//     }
+//     cout << endl;
+// }
+// }
+// int main (){
+//     int t;
+//     cin >> t;
+//     for(int i= 0; i <t; i++){
+//         int n;
+//         cin >> n;
+//         print2(n);
+//     }
+// }
+
+// PATTERN 4
+
+#include<bits/stdc++.h>
+using namespace std;
+void print4(int n){
+    for(int i = 0; i <n; i ++){
+        for(int j= 0; j<=1; j++){
+    cout << endl;
+        }
+}
+}
+int main (){
+    int t;
+    cin >> t;
+    for(int i= 0; i <t; i++){
+        int n;
+        cin >> n;
+        print4(n);
+    }
+}
